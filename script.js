be0ce843a39b68e6918680aab9b561619f1e3f5e
twenty2industries <burak.bostanli.1@web.de>
2025-04-29 21:08:38 +0200

    poke Type one is now displaying correctly

--- script.js
+++ script.js
@@ -16,22 +16,22 @@ let pokeTypesZero = "";
 let pokeTypesOne = "";
 let pokeTypesOnePng = "";
 let pokeTypeZeroPng = "";
+let data = "";
 
 async function loadAllPokemons() {
   let response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
-  let data = await response.json();
+  data = await response.json();
   for (let i = 0; i < data.results.length; i++) {
-    pokeNames = data.results[i].name;
+    pokeNames = data.results[i].name; //
     let pokeUrl = data.results[i].url; //url used with counter to count through all urls.
     let pokeData = await fetch(pokeUrl); // get the data from the URL
     pokeDetails = await pokeData.json(); // parse the fetched Pokémon data
     pokePngs =  pokeDetails.sprites.front_default; // pokemon pictures
 
-    pokeTypesOne = pokeDetails.types[1]?.type?.url || ""; // if got value then define else empty string, variable now ready to fetch get
-    let pokeTypeOneUrlData = await fetch(pokeTypesOne);
-    pokeTypesOnePng = await pokeTypeOneUrlData.text();
-    console.log(pokeTypesOne);
-    
+    pokeTypesOne = pokeDetails.types[1]
+    ? await (await fetch(pokeDetails.types[1].type.url)).json()
+    : undefined;// if got value then define else empty string, variable now ready to fetch get
+    pokeTypesOnePng = pokeTypesOne ? pokeTypesOne.sprites['generation-iii']?.['xd']?.name_icon : ''; // : '' solved an error :undefined occures to be an error in the dom
 
     //fetch url for type pngs 
 
@@ -39,10 +39,12 @@ async function loadAllPokemons() {
     let pokeTypeZeroUrlData = await fetch(pokeTypeZeroUrl); // get data from url
     pokeTypeZeroPng = await pokeTypeZeroUrlData.json(); // define data in variable pokeTypeZero
     pokeTypesZero = pokeTypeZeroPng.sprites['generation-iii']['xd'].name_icon; //type name zero
+
+
     const contentRef = document.getElementById("display-area");
     contentRef.innerHTML += returnDisplays(i);
   }
 }
 
-
 //key = sprites for pngs 
+
